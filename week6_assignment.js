d7abe3e23f4d2e3f921944b0423877d062cf8afd
Kali Campbell <ilak44315@gmail.com>
2022-07-04 15:28:38 -0500

    add assignment rules

---
+++ week6_assignment.js
@@ -0,0 +1,9 @@
+//class
+// Cards
+//Deck
+//Players
+//-	Deal 26 Cards to two Players from a Deck. 
+//-	Iterate through the turns where each Player plays a Card
+//-	The Player who played the higher card is awarded a point
+//o	Ties result in zero points for either Player
+//-	After all cards have been played, display the score.

83b93b9ecc2eb36d043260e523be2df12d26049e
Kali Campbell <ilak44315@gmail.com>
2022-07-04 16:31:16 -0500

    create deck of card file for all uses

--- week6_assignment.js
+++ week6_assignment.js
@@ -7,3 +7,19 @@
 //-	The Player who played the higher card is awarded a point
 //o	Ties result in zero points for either Player
 //-	After all cards have been played, display the score.
+
+class Player{
+    constructor(playerName){
+        this.name = name;
+    }
+}
+class Cards{
+    constructor(cards){
+        this.cards = cardPlayed;
+    }
+}
+class Deck{
+    constructor(deck){
+        this.deck = deck;
+    }
+}
